c30cce64f1b3b586008b8fd096dded3a4839a88a
Arnab Kanti Das <121079578+IamArnab999@users.noreply.github.com>
2024-06-18 01:56:34 +0530

    Update betweenARange.py

--- betweenARange.py
+++ betweenARange.py
@@ -1,5 +1,5 @@
 # question 15 - print all integers between 1 and 50 those are not divisible by 2/3
 
-for i in range(1, 50):
+for i in range(1, 51):
     if i % 2 != 0 and i % 3 != 0:
         print(i)
